fcc362c5cb76146927e7871a4945f8ca7b0a0d2b
Benjaminhofman <benjamin.hofman@hotmail.com>
2025-01-28 09:20:23 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    manifest: true,
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'),
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8888/.netlify/functions',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  plugins: [react()]
 })
