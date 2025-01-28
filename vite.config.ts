f607123dfd29cffe08598bf0c7be02b7c57b7077
Benjaminhofman <benjamin.hofman@hotmail.com>
2025-01-28 09:17:02 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    manifest: true,
     rollupOptions: {
-      input: {
-        main: './index.html'
-      },
+      input: resolve(__dirname, 'index.html'),
       output: {
-        manualChunks: undefined
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
       }
     }
-  },
-  optimizeDeps: {
-    include: ['@stripe/stripe-js']
   },
   server: {
     port: 3000,
