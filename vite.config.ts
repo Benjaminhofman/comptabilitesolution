7a9168ab4171a6a72752c114b2d0cb1a69249436
Benjaminhofman <benjamin.hofman@hotmail.com>
2025-01-28 09:08:29 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
   optimizeDeps: {
     include: ['@stripe/stripe-js']
+  },
+  server: {
+    port: 3000
   }
 })
